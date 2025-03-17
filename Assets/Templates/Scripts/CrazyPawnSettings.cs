4a2191227ea86260b18f950e87bae004b9b8280f
Saina Gulyaeva <sainka.tyan@gmail.com>
2025-03-17 16:46:55 +0300

    Init project

---
+++ Assets/Templates/Scripts/CrazyPawnSettings.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace CrazyPawn
+{
+    [CreateAssetMenu(menuName = "CrazyPawn/Settings", fileName = "CrazyPawnSettings")]
+    public class CrazyPawnSettings : ScriptableObject
+    {
+        [SerializeField] public float InitialZoneRadius = 10f;
+        [SerializeField] public int InitialPawnCount = 7;
+
+        [SerializeField] public Material DeleteMaterial;
+        [SerializeField] public Material ActiveConnectorMaterial;
+
+        [SerializeField] public int CheckerboardSize = 18;
+        [SerializeField] public Color BlackCellColor = Color.yellow;
+        [SerializeField] public Color WhiteCellColor = Color.green;
+    }
+}
